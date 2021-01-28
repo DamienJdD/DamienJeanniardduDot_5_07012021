16a1976daa26f09c3e0473f30547ec02c20ba1f1
Damien Jeanniard <71247094+DamienJdD@users.noreply.github.com>
2021-01-28 17:14:45 +0100

    style: Refonte du style
    - Changement des couleurs du site
    - Logo plus adapté
    - Déplacement des
    <a> dans les <li> plutôt que l'inverse
    - Style du formulaire de
    personnalisation produit (page product.html)

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,28 @@
 // PROMISE
-// FETCH
+// FETCH / AWAIT ASYNC
 // PARCOURIR TON TABLEAU
-// '  "   `
+//
+
+
+fetch("http://localhost:3000/api/furniture")
+    .then(
+        function (response) {
+            return response.json();
+        }
+    )
+    .then(
+        function (response) {
+            for (let i = 0; i < response.length; i++) {
+                let myId = document.getElementById('index-section');
+                let card = `<p>${response[i].name}</p>`;
+                myId.innerHTML += card;
+            }
+
+            return console.log(response);
+        }
+    )
+    .catch(
+        error => console.log("Erreur : " + error)
+    );
+
+console.log('test');
