9321ad0bdf5dd3d882b5b1b1d58bb133e3ff4e5a
Damien Jeanniard <71247094+DamienJdD@users.noreply.github.com>
2021-02-01 22:33:41 +0100

    feat: Création de main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,32 @@
 //
 
 
+// fetch("http://localhost:3000/api/furniture")
+//     .then(
+//         function (response) {
+//             return response.json();
+//         }
+//     )
+//     .then(
+//         function (response) {
+//             for (let i = 0; i < response.length; i++) {
+//                 let myId = document.getElementById('index-section');
+//                 let card = `<p>${response[i].name}</p>`;
+//                 myId.innerHTML += card;
+//             }
+//
+//             return console.log(response);
+//         }
+//     )
+//     .catch(
+//         error => console.log("Erreur : " + error)
+//     );
+//
+//
+// console.log('test');
+
+// mon code à partir de là //
+
 fetch("http://localhost:3000/api/furniture")
     .then(
         function (response) {
@@ -13,9 +39,9 @@ fetch("http://localhost:3000/api/furniture")
     .then(
         function (response) {
             for (let i = 0; i < response.length; i++) {
-                let myId = document.getElementById('index-section');
-                let card = `<p>${response[i].name}</p>`;
-                myId.innerHTML += card;
+                let firstItemNameSpot = document.querySelector('#index-section-list li:first-of-type a div h3');
+                let firstItemName = `${response[i].name}`;
+                firstItemNameSpot.innerHTML = firstItemName;
             }
 
             return console.log(response);
@@ -23,6 +49,4 @@ fetch("http://localhost:3000/api/furniture")
     )
     .catch(
         error => console.log("Erreur : " + error)
-    );
-
-console.log('test');
+    )
